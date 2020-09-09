47fd5a0ea55ff8fe267ca1c15c0e4ba7c0032f3b
delola\delola lokal <aleksandra.man@gmail.com>
2020-09-09 11:08:53 +0200

    Zadanie 3.2. Repozytorium do obsługi encji

--- projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/model/repositories/UserRepository.java
+++ projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/model/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.honestit.spring.demo.model.domain.User;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-	
+
 }
