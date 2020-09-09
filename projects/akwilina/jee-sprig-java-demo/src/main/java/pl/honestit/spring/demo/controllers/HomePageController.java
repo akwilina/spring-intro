d1048adf7ac374854dfc5301f36adaf29e077731
delola\delola lokal <aleksandra.man@gmail.com>
2020-09-09 12:11:24 +0200

    Zadanie 3.4. Metoda do obsługi żądania GET na ścieżkę główną

--- projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
+++ projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
@@ -1,14 +1,30 @@
 package pl.honestit.spring.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import pl.honestit.spring.demo.model.domain.Advert;
+import pl.honestit.spring.demo.model.repositories.AdvertRepository;
 
 @Controller
 @RequestMapping("/")
 @Slf4j @RequiredArgsConstructor
 public class HomePageController {
 
+	private final AdvertRepository advertRepository;
+
+	@GetMapping
+	public String prepareHomePage (Model model) {
+		List<Advert> listOfAllSortedAdverts = advertRepository.findAllByOrderByPostedDesc();
+		
+		model.addAttribute("adverts", listOfAllSortedAdverts );
+		
+		return "home_page";
+	}
 }
--- projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/model/repositories/AdvertRepository.java
+++ projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/model/repositories/AdvertRepository.java
@@ -1,9 +1,13 @@
 package pl.honestit.spring.demo.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.honestit.spring.demo.model.domain.Advert;
 
 public interface AdvertRepository extends JpaRepository<Advert, Long>{
-
+	
+	//ta metoda odwołuje się do bazy danych i na podstawie nazwy metody wnioskowane jet zaptanie do bazy danych
+	List<Advert> findAllByOrderByPostedDesc(); 
 }
