a367d6f0579825620f2dc336bd381c272322e9e9
delola\delola lokal <aleksandra.man@gmail.com>
2020-09-09 11:11:26 +0200

    Zadanie 3.3. Kontroler obsługujący ścieżkę strony głównej

---
+++ projects/akwilina/jee-sprig-java-demo/src/main/java/pl/honestit/spring/demo/controllers/HomePageController.java
@@ -0,0 +1,14 @@
+package pl.honestit.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/")
+@Slf4j @RequiredArgsConstructor
+public class HomePageController {
+
+}
